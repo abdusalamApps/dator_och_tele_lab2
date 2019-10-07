d37673a98280473c4f613d797e9ae20b84636223
abdev <abdusalam.yab1996@gmail.com>
2019-10-07 14:32:03 +0200

    uppgift_f_iii

--- src/MainSimulation.java
+++ src/MainSimulation.java
@@ -9,7 +9,6 @@ public class MainSimulation extends Global {
     public static void main(String[] args) throws IOException {
 
         //Signallistan startas och actSignal deklareras. actSignal är den senast utplockade signalen i huvudloopen nedan.
-
         Signal actSignal;
         new SignalList();
 
@@ -43,43 +42,62 @@ public class MainSimulation extends Global {
 
 
         // Detta är simuleringsloopen:
+        int n = 1;
 
         while (time < 100000) {
 
-            Random random = new Random();
-            int n = 1 + random.nextInt(5);
+//            Random random = new Random();
+//            int n = 1 + random.nextInt(5);
 
-            switch (n) {
+            /*switch (n) {
                 case 1:
                     Generator.sendTo = Q1;
+                    n = 2;
                     break;
                 case 2:
                     Generator.sendTo = Q2;
+                    n = 3;
                     break;
                 case 3:
                     Generator.sendTo = Q3;
+                    n = 4;
                     break;
                 case 4:
                     Generator.sendTo = Q4;
+                    n = 5;
                     break;
                 case 5:
                     Generator.sendTo = Q5;
+                    n = 1;
                     break;
-            }
-
+            }*/
+            Generator.sendTo = findLeastJob(Q1, Q2, Q3, Q4, Q5);
             actSignal = SignalList.FetchSignal();
             time = actSignal.arrivalTime;
             actSignal.destination.TreatSignal(actSignal);
         }
 
         //Slutligen skrivs resultatet av simuleringen ut nedan:
-
         System.out.println("Medelantal kunder i kö 1: " + 1.0 * Q1.accumulated / Q1.noMeasurements);
         System.out.println("Medelantal kunder i kö 2: " + 1.0 * Q2.accumulated / Q2.noMeasurements);
         System.out.println("Medelantal kunder i kö 3: " + 1.0 * Q3.accumulated / Q3.noMeasurements);
         System.out.println("Medelantal kunder i kö 4: " + 1.0 * Q4.accumulated / Q4.noMeasurements);
         System.out.println("Medelantal kunder i kö 5: " + 1.0 * Q5.accumulated / Q5.noMeasurements);
+    }
 
-
+    private static QS findLeastJob(QS q1, QS q2, QS q3, QS q4, QS q5) {
+        List<QS> list = new ArrayList<>();
+        list.add(q1);
+        list.add(q2);
+        list.add(q3);
+        list.add(q4);
+        list.add(q5);
+        QS withLeastJob = q1;
+        for (QS qs : list) {
+        	if (qs.numberInQueue < withLeastJob.numberInQueue) {
+        		withLeastJob = qs;
+			}
+        }
+        return withLeastJob;
     }
 }
