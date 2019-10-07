fb160b9bdefab59913c58ff97b98ba46b63007ff
abdev <abdusalam.yab1996@gmail.com>
2019-10-07 14:04:03 +0200

    uppgift_f_i

--- src/MainSimulation.java
+++ src/MainSimulation.java
@@ -16,29 +16,57 @@ public class MainSimulation extends Global {
         //Här nedan skapas de processinstanser som behövs och parametrar i dem ges värden.
 
         QS Q1 = new QS();
-		QS Q2 = new QS();
-		QS Q3 = new QS();
+        QS Q2 = new QS();
+        QS Q3 = new QS();
+        QS Q4 = new QS();
+        QS Q5 = new QS();
 
-		Q1.sendTo = Q2;
-        Q2.sendTo = Q3;
+        Q1.sendTo = null;
+        Q2.sendTo = null;
         Q3.sendTo = null;
+        Q4.sendTo = null;
+        Q5.sendTo = null;
 
         Gen Generator = new Gen();
-        Generator.lambda = 8; //Generator ska generera nio kunder per sekund
-        Generator.sendTo = Q1; //De genererade kunderna ska skickas till kösystemet QS
+        Generator.lambda = 45; //Generator ska generera nio kunder per sekund
+//        Generator.sendTo = Q1; //De genererade kunderna ska skickas till kösystemet QS
 
         //Här nedan skickas de första signalerna för att simuleringen ska komma igång.
 
         SignalList.SendSignal(READY, Generator, time);
 
         SignalList.SendSignal(MEASURE, Q1, time);
-		SignalList.SendSignal(MEASURE, Q2, time);
-		SignalList.SendSignal(MEASURE, Q3, time);
+        SignalList.SendSignal(MEASURE, Q2, time);
+        SignalList.SendSignal(MEASURE, Q3, time);
+        SignalList.SendSignal(MEASURE, Q4, time);
+        SignalList.SendSignal(MEASURE, Q5, time);
 
 
         // Detta är simuleringsloopen:
 
         while (time < 100000) {
+
+            Random random = new Random();
+            int n = 1 + random.nextInt(5);
+
+            switch (n) {
+                case 1:
+                    Generator.sendTo = Q1;
+                    break;
+                case 2:
+                    Generator.sendTo = Q2;
+                    break;
+                case 3:
+                    Generator.sendTo = Q3;
+                    break;
+                case 4:
+                    Generator.sendTo = Q4;
+                    break;
+                case 5:
+                    Generator.sendTo = Q5;
+                    break;
+            }
+
             actSignal = SignalList.FetchSignal();
             time = actSignal.arrivalTime;
             actSignal.destination.TreatSignal(actSignal);
@@ -47,8 +75,11 @@ public class MainSimulation extends Global {
         //Slutligen skrivs resultatet av simuleringen ut nedan:
 
         System.out.println("Medelantal kunder i kö 1: " + 1.0 * Q1.accumulated / Q1.noMeasurements);
-		System.out.println("Medelantal kunder i kö 2: " + 1.0 * Q2.accumulated / Q2.noMeasurements);
-		System.out.println("Medelantal kunder i kö 3: " + 1.0 * Q3.accumulated / Q3.noMeasurements);
+        System.out.println("Medelantal kunder i kö 2: " + 1.0 * Q2.accumulated / Q2.noMeasurements);
+        System.out.println("Medelantal kunder i kö 3: " + 1.0 * Q3.accumulated / Q3.noMeasurements);
+        System.out.println("Medelantal kunder i kö 4: " + 1.0 * Q4.accumulated / Q4.noMeasurements);
+        System.out.println("Medelantal kunder i kö 5: " + 1.0 * Q5.accumulated / Q5.noMeasurements);
+
 
     }
 }
